886d0277c46a94d39c5593248053aed7eb88215e
Bryan Alvarez <balvarez-2023244@kinal.edu.gt>
2024-07-20 01:21:02 -0600

    El CRUD en las interfaces

---
+++ CRUDProducto/src/main/java/com/bryanalvarez/crudproducto/service/PorductoService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bryanalvarez.crudproducto.service;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class PorductoService {
+    
+}
